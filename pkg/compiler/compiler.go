06c8773c735164b2e345dc35fcff37514fcff45c
brenoafb <breno.afb@gmail.com>
2023-08-13 22:42:17 -0300

    add builtins to procedure map

--- pkg/compiler/compiler.go
+++ pkg/compiler/compiler.go
@@ -66,6 +66,70 @@ func (c *Compiler) preprocess(expr interface{}) (interface{}, error) {
 	}, nil
 }
 
+func (c *Compiler) annotateFreeVariables(
+	expr interface{},
+	args map[string]struct{},
+	freeVars map[string]struct{},
+) ([]string, error) {
+	switch expr.(type) {
+	case string:
+		// TODO
+		return nil, nil
+	case []interface{}:
+		elems := expr.([]interface{})
+		if len(elems) == 0 {
+			return nil, nil
+		}
+
+		head := elems[0]
+
+		if head == "lambda" {
+			if len(elems) != 3 {
+				return nil, fmt.Errorf("lambda form must contain 3 elements")
+			}
+
+			// args := elems[1].([]interface{})
+		}
+	}
+
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (c *Compiler) gatherFreeVariables(
+	expr interface{},
+	args map[string]struct{},
+	freeVars map[string]struct{},
+) error {
+	switch expr.(type) {
+	case string:
+		v := expr.(string)
+		if _, ok := args[v]; ok {
+			return nil
+		}
+
+		freeVars[v] = struct{}{}
+
+		return nil
+
+	case []interface{}:
+		elems := expr.([]interface{})
+		if len(elems) == 0 {
+			return nil
+		}
+
+		for _, elem := range elems {
+			err := c.gatherFreeVariables(elem, args, freeVars)
+			if err != nil {
+				return fmt.Errorf("error gathering free vars from subexpression: %w", err)
+			}
+		}
+	default:
+		return nil
+	}
+
+	return nil
+}
+
 func (c *Compiler) compileExpr(expr interface{}) error {
 	switch expr.(type) {
 	case string:
@@ -100,506 +164,11 @@ func (c *Compiler) compileExpr(expr interface{}) error {
 
 		head := elems[0]
 
-		if head == "add1" {
-			x := elems[1]
-			err := c.compileExpr(x)
-			if err != nil {
-				return fmt.Errorf("error compiling '%s' application: %w", "add1", err)
+		switch head.(type) {
+			case string:
+			if proc, ok := builtins[head.(string)]; ok {
+				return proc(c, elems)
 			}
-			c.emit("addl $4, %eax")
-			return nil
-		}
-
-		if head == "integer->char" {
-			x := elems[1]
-			err := c.compileExpr(x)
-			if err != nil {
-				return fmt.Errorf("error compiling '%s' application: %w", "integer->char", err)
-			}
-			c.emit(fmt.Sprintf("sall $%d, %%eax", charShift-fixnumShift))
-			c.emit(fmt.Sprintf("orl $0x%x, %%eax", charTag))
-			return nil
-		}
-
-		if head == "char->integer" {
-			x := elems[1]
-			err := c.compileExpr(x)
-			if err != nil {
-				return fmt.Errorf("error compiling '%s' application: %w", "char->integer", err)
-			}
-			c.emit(fmt.Sprintf("sarl $%d, %%eax", charShift-fixnumShift))
-			return nil
-		}
-
-		if head == "null?" {
-			x := elems[1]
-			err := c.compileExpr(x)
-			if err != nil {
-				return fmt.Errorf("error compiling '%s' application: %w", "zero?", err)
-			}
-			c.emit(fmt.Sprintf("cmpl $0x%x, %%eax", emptyList))
-			c.emit("movl $0, %eax")
-			c.emit("sete %al")
-			c.emit("sall $7, %eax")
-			c.emit(fmt.Sprintf("orl $0x%x, %%eax", boolTag))
-
-			return nil
-		}
-
-		if head == "zero?" {
-			x := elems[1]
-			err := c.compileExpr(x)
-			if err != nil {
-				return fmt.Errorf("error compiling '%s' application: %w", "zero?", err)
-			}
-			c.emit("cmpl $0, %eax")
-			c.emit("movl $0, %eax")
-			c.emit("sete %al")
-			c.emit("sall $7, %eax")
-			c.emit(fmt.Sprintf("orl $0x%x, %%eax", boolTag))
-
-			return nil
-		}
-
-		if head == "+" {
-			x := elems[1]
-			y := elems[2]
-			err := c.compileExpr(y)
-			if err != nil {
-				return fmt.Errorf("error compiling '%s' application: %w", "+", err)
-			}
-			c.push()
-			err = c.compileExpr(x)
-			c.si += wordsize
-			if err != nil {
-				return fmt.Errorf("error compiling '%s' application: %w", "+", err)
-			}
-			c.emit(fmt.Sprintf("addl %d(%%esp), %%eax", c.si))
-
-			return nil
-		}
-
-		if head == "-" {
-			x := elems[1]
-			y := elems[2]
-			err := c.compileExpr(y)
-			if err != nil {
-				return fmt.Errorf("error compiling '%s' application: %w", "+", err)
-			}
-			c.push()
-			err = c.compileExpr(x)
-			c.si += wordsize
-			if err != nil {
-				return fmt.Errorf("error compiling '%s' application: %w", "+", err)
-			}
-			c.emit(fmt.Sprintf("subl %d(%%esp), %%eax", c.si))
-
-			return nil
-		}
-
-		if head == "let" {
-			// (let <bindings...> <body>)
-			if len(elems) < 3 {
-				panic("invalid let form")
-			}
-			si := c.si
-			bindings := elems[1 : len(elems)-1]
-			body := elems[len(elems)-1]
-
-			// each binding has form
-			// (<variable> <body>)
-			for _, binding := range bindings {
-				xs := binding.([]interface{})
-				v := xs[0].(string)
-				b := xs[1]
-				err := c.compileExpr(b)
-				if err != nil {
-					return fmt.Errorf("error compiling let binding: %w", err)
-				}
-				idx := c.si
-				c.push()
-				c.env[v] = location{
-					location: stack,
-					offset:   idx,
-				}
-			}
-
-			err := c.compileExpr(body)
-			if err != nil {
-				return fmt.Errorf("error compiling let binding body: %w", err)
-			}
-
-			c.si = si
-
-			return nil
-		}
-
-		if head == "if" {
-			if len(elems) != 4 {
-				return fmt.Errorf("malformed 'if' expression")
-			}
-			test := elems[1]
-			conseq := elems[2]
-			alt := elems[3]
-
-			l0 := c.genLabel()
-			l1 := c.genLabel()
-
-			err := c.compileExpr(test)
-			if err != nil {
-				return fmt.Errorf("error compiling test in if expression: %w", err)
-			}
-			c.emit(fmt.Sprintf("cmpl $0x%x, %%eax", immFalse))
-			c.emit(fmt.Sprintf("je %s", l0))
-
-			err = c.compileExpr(conseq)
-			if err != nil {
-				return fmt.Errorf("error compiling conseq in if expression: %w", err)
-			}
-
-			c.emit(fmt.Sprintf("jmp %s", l1))
-			c.emit(fmt.Sprintf("%s:", l0))
-
-			err = c.compileExpr(alt)
-			if err != nil {
-				return fmt.Errorf("error compiling alt in if expression: %w", err)
-			}
-
-			c.emit(fmt.Sprintf("%s:", l1))
-		}
-
-		if head == "cons" {
-			if len(elems) != 3 {
-				return fmt.Errorf("malformed cons expression")
-			}
-			x := elems[1]
-			y := elems[2]
-
-			err := c.compileExpr(x)
-			if err != nil {
-				return fmt.Errorf("error compiling cons expression: %w", err)
-			}
-			c.emit(fmt.Sprintf("movl %%eax, %d(%%esi)", 0*wordsize))
-
-			err = c.compileExpr(y)
-			if err != nil {
-				return fmt.Errorf("error compiling cons expression: %w", err)
-			}
-			c.emit(fmt.Sprintf("movl %%eax, %d(%%esi)", 1*wordsize))
-
-			c.emit("movl %esi, %eax")
-			c.emit("orl $1, %eax")
-
-			c.emit(fmt.Sprintf("addl $%d, %%esi", 2*wordsize))
-
-			return nil
-		}
-
-		if head == "car" {
-			if len(elems) != 2 {
-				return fmt.Errorf("malformed car expression")
-			}
-			err := c.compileExpr(elems[1])
-			if err != nil {
-				return fmt.Errorf("error compiling car expression: %w", err)
-			}
-
-			c.emit("movl -1(%eax), %eax")
-			return nil
-		}
-
-		if head == "cdr" {
-			if len(elems) != 2 {
-				return fmt.Errorf("malformed cdr expression")
-			}
-			err := c.compileExpr(elems[1])
-			if err != nil {
-				return fmt.Errorf("error compiling car expression: %w", err)
-			}
-
-			c.emit(fmt.Sprintf("movl %d(%%eax), %%eax", wordsize-1))
-			return nil
-		}
-
-		if head == "make-vector" {
-			if len(elems) != 2 {
-				return fmt.Errorf("malformed make-vector expression")
-			}
-
-			err := c.compileExpr(elems[1])
-
-			if err != nil {
-				return fmt.Errorf("error compiling make-vector call: %w", err)
-			}
-
-			// set length
-			c.emit(fmt.Sprintf("movl %%eax, 0(%%esi)"))
-			// save length
-			c.emit(fmt.Sprintf("movl %%eax, %%ebx"))
-			// eax = esi | 2
-			c.emit(fmt.Sprintf("movl %%esi, %%eax"))
-			c.emit(fmt.Sprintf("orl $2, %%eax")) // 2 = vector tag
-			// align size to next object boundary
-			c.emit(fmt.Sprintf("addl $11, %%ebx"))
-			c.emit(fmt.Sprintf("andl $-8, %%ebx"))
-			// advance alloc ptr
-			c.emit(fmt.Sprintf("addl %%ebx, %%esi"))
-			return nil
-		}
-
-		if head == "vector-ref" {
-			if len(elems) != 3 {
-				return fmt.Errorf("vector-ref requires 2 parameters")
-			}
-
-			vector := elems[1]
-			idx := elems[2]
-
-			err := c.compileExpr(vector)
-			if err != nil {
-				return fmt.Errorf("error compiling vector expr in vector-ref call: %w", err)
-			}
-
-			// save vector ptr
-			vectorIdx := c.si
-			c.push()
-
-			err = c.compileExpr(idx)
-			if err != nil {
-				return fmt.Errorf("error compiling index expr in vector-ref call: %w", err)
-			}
-
-			c.emit("addl $1, %eax")
-			c.emit(fmt.Sprintf("movl %d(%%esp), %%ebx", vectorIdx))
-			c.emit("addl %ebx, %eax")
-			c.emit("movl 0(%eax), %eax")
-
-			return nil
-		}
-
-		if head == "vector-set!" {
-			if len(elems) != 4 {
-				return fmt.Errorf("vector-set requires 3 parameters")
-			}
-
-			vector := elems[1]
-			idx := elems[2]
-			obj := elems[3]
-
-			err := c.compileExpr(vector)
-			if err != nil {
-				return fmt.Errorf("error compiling vector expr in vector-set! call: %w", err)
-			}
-
-			// save vector ptr
-			vectorIdx := c.si
-			c.push()
-
-			err = c.compileExpr(idx)
-			if err != nil {
-				return fmt.Errorf("error compiling index expr in vector-set! call: %w", err)
-			}
-
-			// save idx
-			idxIdx := c.si
-			c.push()
-
-			err = c.compileExpr(obj)
-			if err != nil {
-				return fmt.Errorf("error compiling object expr in vector-set! call: %w", err)
-			}
-
-			// compute destination pointer
-			c.emit(fmt.Sprintf("movl %d(%%esp), %%ebx", idxIdx))
-			c.emit("addl $1, %ebx")
-			c.emit(fmt.Sprintf("addl %d(%%esp), %%ebx", vectorIdx))
-			// move object into slot
-			c.emit("movl %eax, 0(%ebx)")
-			// set eax to vector pointer
-			c.emit(fmt.Sprintf("movl %d(%%esp), %%eax", vectorIdx))
-
-			return nil
-		}
-
-		if head == "labels" {
-			if len(elems) != 3 {
-				return fmt.Errorf("labels form must contain 3 elements")
-			}
-
-			lvars := elems[1].([]interface{})
-			body := elems[2]
-
-			err := c.compileExpr(body)
-			if err != nil {
-				return fmt.Errorf("error compiling body in labels form: %w", err)
-			}
-
-			c.emit("ret")
-
-			for i, lvar := range lvars {
-				pair := lvar.([]interface{})
-				if len(pair) != 2 {
-					return fmt.Errorf("bad lvar in label form at index %d", i)
-				}
-				name := pair[0].(string)
-				lvarBody := pair[1]
-
-				c.emit(fmt.Sprintf("%s:", name))
-
-				err := c.compileExpr(lvarBody)
-				if err != nil {
-					return fmt.Errorf("error compiling body in labels form: %w")
-				}
-			}
-		}
-
-		if head == "code" {
-			if len(elems) != 4 {
-				return fmt.Errorf("'code' form must contain 3 parameters")
-			}
-
-			arglist := elems[1].([]interface{})
-			freevars := elems[2].([]interface{})
-			body := elems[3]
-
-			// assign stack location for each argument
-			for i, arg := range arglist {
-				v := arg.(string)
-				c.env[v] = location{
-					location: stack,
-					offset:   -wordsize * (i + 1),
-				}
-			}
-
-			// adjust the stack so that we point on top
-			// of the arguments
-			c.si = -wordsize * (len(arglist) + 1)
-
-			// assign closure location for each free variable
-			for i, arg := range freevars {
-				fv := arg.(string)
-				c.env[fv] = location{
-					location: closure,
-					offset:   -wordsize * (i + 1),
-				}
-			}
-
-			err := c.compileExpr(body)
-			if err != nil {
-				return fmt.Errorf("error compiling body in 'code' form: %w")
-			}
-
-			c.emit("ret")
-			c.clearEnv()
-			c.si = -wordsize
-			return nil
-		}
-
-		if head == "labelcall" {
-			if len(elems) < 2 {
-				return fmt.Errorf("labelcall form must contain at least 1 parameter")
-			}
-
-			l := elems[1].(string)
-			spSlot := c.si + wordsize
-			siBefore := c.si
-			// skip one slot for the return address
-			c.si -= wordsize
-			for i, arg := range elems[2:] {
-				err := c.compileExpr(arg)
-				if err != nil {
-					return fmt.Errorf("error compiling argument at index %d in labelcall: %w", i, err)
-				}
-				c.push()
-			}
-			// handle call and return
-			// call subtracts wordsize from esp, so we need to adjust it first
-			// to make sure we don't overwrite local variables
-			c.emit(fmt.Sprintf("addl $%d, %%esp", spSlot))
-			c.emit(fmt.Sprintf("call %s", l))
-			// restore esp
-			c.emit(fmt.Sprintf("addl $%d, %%esp", -spSlot))
-			c.si = siBefore
-
-			return nil
-		}
-
-		if head == "funcall" {
-			if len(elems) < 2 {
-				return fmt.Errorf("funcall form must contain at least 1 parameter")
-			}
-
-			f := elems[1]
-
-			spSlot := c.si
-			siBefore := c.si
-			// skip two slots for the return address and closure pointer
-			c.si -= 2 * wordsize
-			for i, arg := range elems[2:] {
-				err := c.compileExpr(arg)
-				if err != nil {
-					return fmt.Errorf("error compiling argument at index %d in funcall: %w", i, err)
-				}
-				c.push()
-			}
-
-			// save closure pointer
-			c.emit(fmt.Sprintf("movl %%edi, %d(%%esp)", siBefore))
-
-			err := c.compileExpr(f)
-			if err != nil {
-				return fmt.Errorf("error compiling function in funcall: %w", err)
-			}
-
-			// move new closure into closure pointer
-			c.emit("movl %eax, %edi")
-			// clear tag
-			c.emit("andl $-8, %edi")
-
-			// handle call and return
-			c.emit("movl 0(%edi), %ebx")
-			c.emit(fmt.Sprintf("addl $%d, %%esp", spSlot))
-			c.emit("call *%ebx")
-			c.emit(fmt.Sprintf("addl $%d, %%esp", -spSlot))
-			c.si = siBefore
-
-			return nil
-		}
-
-		if head == "closure" {
-			if len(elems) < 2 {
-				return fmt.Errorf("closure form must contain at least 1 parameter")
-			}
-
-			l := elems[1].(string)
-			c.emit(fmt.Sprintf("movl $%s, 0(%%esi)", l))
-			for i, freevar := range elems[2:] {
-				// TODO copy free variable value directly instead
-				// of moving to eax then to heap
-				err := c.compileExpr(freevar.(string))
-				if err != nil {
-					return fmt.Errorf(
-						"error compiling free variable in closure form at index %d: %w",
-						i,
-						err,
-					)
-				}
-
-				c.emit(fmt.Sprintf("movl %%eax, %d(%%esi)", wordsize*(i+1)))
-			}
-
-			length := len(elems) - 2
-
-			c.emit(fmt.Sprintf("movl $%d, %%ebx", length))
-			// eax = esi | 6
-			c.emit(fmt.Sprintf("movl %%esi, %%eax"))
-			c.emit(fmt.Sprintf("orl $6, %%eax")) // 6 = closure tag
-			// align size to next object boundary
-			c.emit(fmt.Sprintf("addl $11, %%ebx"))
-			c.emit(fmt.Sprintf("andl $-8, %%ebx"))
-			// advance alloc ptr
-			c.emit(fmt.Sprintf("addl %%ebx, %%esi"))
-			return nil
 		}
 
 		return fmt.Errorf("unsupported operation %s", head)
--- pkg/compiler/compiler_test.go
+++ pkg/compiler/compiler_test.go
@@ -3,9 +3,11 @@ package compiler
 import (
 	"bytes"
 	"fmt"
-
-	// "fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/brenoafb/tinycompiler/pkg/parser"
 )
 
 func TestIntegerExpr(t *testing.T) {
@@ -154,5 +156,60 @@ ret
 
 	if w.String() != expected {
 		t.Errorf("emmited code did not match expected output")
+	}
+}
+
+func TestGatherFreeVariables(t *testing.T) {
+	tests := []struct {
+		code             string
+		expectedFreeVars []string
+		args             []string
+	}{
+		{
+			code:             "(a b c x y z a b c)",
+			expectedFreeVars: []string{"x", "y", "z"},
+			args:             []string{"a", "b", "c"},
+		},
+		{
+			code:             "(a (b c) x y (z a) b c)",
+			expectedFreeVars: []string{"x", "y", "z"},
+			args:             []string{"a", "b", "c"},
+		},
+		{
+			code:             "((a b) c (x y z) a b c)",
+			expectedFreeVars: []string{"x", "y", "z"},
+			args:             []string{"a", "b", "c"},
+		},
+		{
+			code:             "(a (b (c (x y) z) a) b c)",
+			expectedFreeVars: []string{"x", "y", "z"},
+			args:             []string{"a", "b", "c"},
+		},
+		// Additional test cases can be added here
+	}
+
+	w := &bytes.Buffer{}
+	c := NewCompiler(w)
+
+	for _, tt := range tests {
+		t.Run(tt.code, func(t *testing.T) { // Use the code as the descriptor
+			// Convert the slice of args into a map
+			argsMap := make(map[string]struct{})
+			for _, arg := range tt.args {
+				argsMap[arg] = struct{}{}
+			}
+
+			tokens := parser.Tokenize(tt.code)
+			expr, err := parser.Parse(tokens)
+			require.NoError(t, err)
+
+			freeVars := map[string]struct{}{}
+			err = c.gatherFreeVariables(expr, argsMap, freeVars)
+			require.NoError(t, err)
+
+			for _, key := range tt.expectedFreeVars {
+				require.Contains(t, freeVars, key, "Free vars should contain "+key)
+			}
+		})
 	}
 }
